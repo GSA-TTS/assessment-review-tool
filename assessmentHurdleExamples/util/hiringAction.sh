7418eddb1c2ec628c2a530998635d390c0621d19
Julius Skyler Slade <julius.s.slade@omb.eop.gov>
2025-01-30 18:04:08 +0000

    Validate hiring action JSON before attempting to create

--- assessmentHurdleExamples/util/hiringAction.sh
+++ assessmentHurdleExamples/util/hiringAction.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-# Called from parent of `util`
-# expects the following env vars to be set:
+# Called from parent of `util`.
+# Does not expect applicants.csv to exist in AGENCY_DIR; you can add applicants later.
+# Expects the following env vars to be set:
 
 : "${HOST?Need to set HOST}";
 : "${TOKEN?Need to set TOKEN}";
 : "${AGENCY_DIR?Need to set AGENCY_DIR}";
+
+if ! /bin/bash ./util/checkJSON.sh "$AGENCY_DIR"; then
+    echo "Exiting because of bad JSON in AGENCY_DIR"
+    exit 1;
+fi
 
 printf "HOST: $HOST\nTOKEN: $TOKEN\nAGENCY_DIR: $AGENCY_DIR\n";
 
@@ -14,9 +20,11 @@ HA_ID=`curl --silent -X PUT -H "Authorization: token ${TOKEN}" -H "Content-Type:
 if [[ $HA_ID == null ]]; 
 
 then 
+    printf "\nFailed to create a new hiring action";
     printf "\nNo hiring action ID";
     exit 1; 
-fi;
+fi
+
 # Remove leading `"` 
 HA_ID="${HA_ID%\"}" 
 # Remove trailing `"``
@@ -35,6 +43,6 @@ export USERS_FILE=${AGENCY_DIR}/users.json
 # with the correct env vars.
 /bin/bash ./util/addUsers.sh && \
 /bin/bash ./util/addSpecialties.sh && \
-/bin/bash ./util/addApplicants.sh && \
+/bin/test -f "$APPLICANTS_FILE" && /bin/bash ./util/addApplicants.sh \
 printf "\n\nHiring action successfully created!" && exit;
 printf "\n\nHiring action creation failed";
